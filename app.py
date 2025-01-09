660e25613e5b3187b439ccec9e37b806b3815de7
Deivid Smek <57196475+deividsmek@users.noreply.github.com>
2025-01-08 22:05:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+!pip install dash dash-bootstrap-components pandas
+
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
@@ -5,13 +7,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime, timedelta
-from google.colab import drive
-
-# Montar o Google Drive
-drive.mount('/content/drive')
 
 # Nome do arquivo CSV onde os dados serão armazenados
-csv_file = '/content/drive/My Drive/vendas_diarias.csv'
+csv_file = 'vendas_diarias.csv'
 
 # Meta de vendas para o ano
 meta_vendas = 50000  # Meta final de vendas é 50.000 reais
@@ -24,7 +22,8 @@ def carregar_dados():
     try:
         df = pd.read_csv(csv_file, parse_dates=['Data'])
     except FileNotFoundError:
-        df = pd.DataFrame({'Data': dias_do_ano, 'Vendas': [0] * len(dias_do_ano)})
+        df = pd.DataFrame({'Data': dias_do_ano, 'Vendas Produtos Físicos': [0] * len(dias_do_ano),
+                           'Vendas Produtos Digitais': [0] * len(dias_do_ano), 'Total Vendas': [0] * len(dias_do_ano)})
         salvar_dados(df)  # Salvar o arquivo se não existir
     return df
 
@@ -33,23 +32,27 @@ def salvar_dados(df):
     df.to_csv(csv_file, index=False)
 
 # Função para inserir vendas diárias
-def inserir_vendas(valor):
+def inserir_vendas(vendas_fisicas, vendas_digitais):
     df = carregar_dados()
     hoje = datetime.now().date()
     if hoje in df['Data'].dt.date.values:
-        df.loc[df['Data'].dt.date == hoje, 'Vendas'] = valor
+        df.loc[df['Data'].dt.date == hoje, 'Vendas Produtos Físicos'] = vendas_fisicas
+        df.loc[df['Data'].dt.date == hoje, 'Vendas Produtos Digitais'] = vendas_digitais
+        df.loc[df['Data'].dt.date == hoje, 'Total Vendas'] = vendas_fisicas + vendas_digitais
         salvar_dados(df)
-        print(f"Valor de {valor} inserido para o dia {hoje}.")
+        print(f"Vendas inseridas para o dia {hoje}.")
     else:
         print("Erro: Data fora do intervalo definido.")
 
 # Função para atualizar vendas de um dia específico
-def atualizar_vendas(dia, valor):
+def atualizar_vendas(dia, vendas_fisicas, vendas_digitais):
     df = carregar_dados()
     if dia in df['Data'].dt.dayofyear.values:
-        df.loc[df['Data'].dt.dayofyear == dia, 'Vendas'] = valor
+        df.loc[df['Data'].dt.dayofyear == dia, 'Vendas Produtos Físicos'] = vendas_fisicas
+        df.loc[df['Data'].dt.dayofyear == dia, 'Vendas Produtos Digitais'] = vendas_digitais
+        df.loc[df['Data'].dt.dayofyear == dia, 'Total Vendas'] = vendas_fisicas + vendas_digitais
         salvar_dados(df)
-        print(f"Valor de {valor} inserido para o dia {dia}.")
+        print(f"Vendas atualizadas para o dia {dia}.")
     else:
         print("Erro: Dia fora do intervalo definido.")
 
@@ -57,7 +60,7 @@ def atualizar_vendas(dia, valor):
 def calcular_meta_diaria_restante(df):
     hoje = datetime.now().date()
     dias_restantes = (dias_do_ano[-1].date() - hoje).days + 1
-    progresso_acumulado = df['Vendas'].sum()
+    progresso_acumulado = df['Total Vendas'].sum()
     meta_restante = meta_vendas - progresso_acumulado
     if dias_restantes > 0:
         return meta_restante / dias_restantes
@@ -80,20 +83,24 @@ app.layout = html.Div([
     html.H1("Dashboard de Vendas 2025", style={'textAlign': 'center'}),
 
     html.Div([
+        html.H3(obter_data_atual(), style={'margin-bottom': '20px'}),
         html.Div([
-            html.H3(obter_data_atual(), style={'margin-bottom': '20px'}),
-            html.Label('Valor de venda de hoje:'),
-            dcc.Input(id='input-vendas', type='number', placeholder='Insira o valor de vendas', value=0, style={'margin-right': '10px'}),
-            html.Button(id='submit-button', n_clicks=0, children='Inserir Vendas')
-        ], style={'display': 'inline-block', 'padding': '10px'}),
+            html.Label('Vendas Produtos Físicos:', style={'gridColumn': '1'}),
+            dcc.Input(id='input-vendas-fisicas', type='number', placeholder='Insira o valor de vendas físicas', value=0, style={'gridColumn': '2'}),
+            html.Label('Vendas Produtos Digitais:', style={'gridColumn': '3'}),
+            dcc.Input(id='input-vendas-digitais', type='number', placeholder='Insira o valor de vendas digitais', value=0, style={'gridColumn': '4'}),
+            html.Button(id='submit-button', n_clicks=0, children='Inserir Vendas', style={'gridColumn': '5'})
+        ], style={'display': 'grid', 'gridTemplateColumns': 'auto auto auto auto auto', 'gap': '10px', 'padding': '10px'}),
 
         html.Div([
-            html.Label('Dia do ano que deseja alterar:'),
-            dcc.Input(id='dia', type='number', placeholder='Número do dia (1-365)', value=1, style={'margin-right': '10px'}),
-            html.Label('Valor a ser alterado:'),
-            dcc.Input(id='valor-dia', type='number', placeholder='Valor para o dia específico', value=0, style={'margin-right': '10px'}),
-            html.Button(id='update-button', n_clicks=0, children='Atualizar Vendas de Dia Específico')
-        ], style={'display': 'inline-block', 'padding': '10px'})
+            html.Label('Dia do ano que deseja alterar:', style={'gridColumn': '1'}),
+            dcc.Input(id='dia', type='number', placeholder='Número do dia (1-365)', value=1, style={'gridColumn': '2'}),
+            html.Label('Vendas Produtos Físicos:', style={'gridColumn': '3'}),
+            dcc.Input(id='valor-dia-fisicas', type='number', placeholder='Valor para o dia específico', value=0, style={'gridColumn': '4'}),
+            html.Label('Vendas Produtos Digitais:', style={'gridColumn': '5'}),
+            dcc.Input(id='valor-dia-digitais', type='number', placeholder='Valor para o dia específico', value=0, style={'gridColumn': '6'}),
+            html.Button(id='update-button', n_clicks=0, children='Atualizar Vendas de Dia Específico', style={'gridColumn': '7'})
+        ], style={'display': 'grid', 'gridTemplateColumns': 'auto auto auto auto auto auto auto', 'gap': '10px', 'padding': '10px'})
     ], style={'textAlign': 'center'}),
 
     html.Div(id='mensagem-sucesso', style={'textAlign': 'center', 'color': 'green'}),
@@ -120,22 +127,25 @@ app.layout = html.Div([
      Output('meta-diaria', 'figure'),
      Output('valor-acumulado', 'children'),
      Output('meta-diaria-atual', 'children'),
-     Output('input-vendas', 'value'),
+     Output('input-vendas-fisicas', 'value'),
+     Output('input-vendas-digitais', 'value'),
      Output('mensagem-sucesso', 'children')],
     [Input('submit-button', 'n_clicks'),
      Input('update-button', 'n_clicks')],
-    [State('input-vendas', 'value'),
+    [State('input-vendas-fisicas', 'value'),
+     State('input-vendas-digitais', 'value'),
      State('dia', 'value'),
-     State('valor-dia', 'value')]
+     State('valor-dia-fisicas', 'value'),
+     State('valor-dia-digitais', 'value')]
 )
-def update_graphs(n_clicks_insert, n_clicks_update, valor_vendas, dia, valor_dia):
+def update_graphs(n_clicks_insert, n_clicks_update, vendas_fisicas, vendas_digitais, dia, valor_dia_fisicas, valor_dia_digitais):
     mensagem_sucesso = ""
-    if n_clicks_insert > 0 and valor_vendas is not None:
-        inserir_vendas(valor_vendas)
+    if n_clicks_insert > 0 and vendas_fisicas is not None and vendas_digitais is not None:
+        inserir_vendas(vendas_fisicas, vendas_digitais)
         mensagem_sucesso = "Vendas inseridas com sucesso!"
 
-    if n_clicks_update > 0 and dia is not None and valor_dia is not None:
-        atualizar_vendas(dia, valor_dia)
+    if n_clicks_update > 0 and dia is not None and valor_dia_fisicas is not None and valor_dia_digitais is not None:
+        atualizar_vendas(dia, valor_dia_fisicas, valor_dia_digitais)
         mensagem_sucesso = "Vendas atualizadas com sucesso!"
 
     # Recarregar os dados do CSV
@@ -145,12 +155,12 @@ def update_graphs(n_clicks_insert, n_clicks_update, valor_vendas, dia, valor_dia
     meta_diaria_restante = calcular_meta_diaria_restante(df)
 
     # Gráfico de barras das vendas diárias
-    fig_diario = px.bar(df, x='Data', y='Vendas',
-                        labels={'Data': 'Dias do Ano', 'Vendas': 'Vendas Diárias'},
+    fig_diario = px.bar(df, x='Data', y='Total Vendas',
+                        labels={'Data': 'Dias do Ano', 'Total Vendas': 'Vendas Diárias'},
                         title='Progresso Diário das Vendas')
 
     # Gráfico de pizza do progresso anual
-    progresso_acumulado = df['Vendas'].sum()
+    progresso_acumulado = df['Total Vendas'].sum()
     progresso_percentual = progresso_acumulado / meta_vendas * 100
     fig_anual = px.pie(values=[progresso_percentual, 100 - progresso_percentual],
                        names=['Progresso', 'Restante'],
@@ -170,7 +180,7 @@ def update_graphs(n_clicks_insert, n_clicks_update, valor_vendas, dia, valor_dia
     ))
     fig_meta_diaria.add_trace(go.Bar(
         x=ultimos_7_dias['Data'],
-        y=ultimos_7_dias['Vendas'],
+        y=ultimos_7_dias['Total Vendas'],
         name='Vendas',
         marker_color='blue'
     ))
@@ -185,7 +195,7 @@ def update_graphs(n_clicks_insert, n_clicks_update, valor_vendas, dia, valor_dia
     valor_acumulado_text = f"Valor Acumulado: R$ {progresso_acumulado:,.2f}"
     meta_diaria_atual_text = f"Meta Diária Atual: R$ {meta_diaria_restante:,.2f}"
 
-    return fig_diario, fig_anual, fig_meta_diaria, valor_acumulado_text, meta_diaria_atual_text, 0, mensagem_sucesso
+    return fig_diario, fig_anual, fig_meta_diaria, valor_acumulado_text, meta_diaria_atual_text, 0, 0, mensagem_sucesso
 
 if __name__ == '__main__':
     app.run_server(debug=True)
